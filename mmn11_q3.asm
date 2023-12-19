1670954d995e6b5f5d1560690ef2208a8ff63af5
noam <noomta10@gmail.com>
2023-12-19 15:29:24 +0200

    continuee D

--- mmn11_q3.asm
+++ mmn11_q3.asm
@@ -5,7 +5,7 @@
 # $t3 = placeholder to store a bit
 # $t4, $t5 = copy of user's number 
 # $t6 = stores the decimal value of the reverse binary
-
+# $t7 = mask to extract bits for decimal print
 
 .data
     prompt: .asciiz "Enter a number between -9999 and 9999:\n"
@@ -24,31 +24,25 @@
         # Get the user's number
         li $v0, 5 
         syscall
-        # Store the number in $t0
+        
+        # Store the number in $t0, $t4 and $t5
         move $t0, $v0
-        # Store the number in $t4 and $t5
         move $t4, $t0
         move $t5, $t0
-        # Branch if less than -9999
-        blt $t0, -9999, invalid_input
-	# Branch if grater than 9999
-        bgt $t0, 9999, invalid_input
+                
+        blt $t0, -9999, invalid_input  # Branch if less than -9999	
+        bgt $t0, 9999, invalid_input   # Branch if grater than 9999
       
      
      set_variables:
-        # Counter for 16 bits
-        li $t1, 16
-        # Mask to extract each bit
-        li $t2, 32768 
+        li $t1, 16      # Counter for 16 bits  
+        li $t2, 0x8000  # Mask to extract each bit
         
      
-    print_binary:
-        # Extract a bit using AND operation
-        and $t3, $t0, $t2
-        # Check if bit is equal to zero
-        beqz $t3, load_zero
-        # If the bit value is not zero, load one
-    	li $a0, '1'
+    print_binary_loop:      
+        and $t3, $t0, $t2    # Extract a bit using AND operation       
+        beqz $t3, load_zero  # Check if bit is equal to zero        
+    	li $a0, '1'          # If the bit value is not zero, load one
      	j print_bit
         
 
@@ -59,13 +53,10 @@
     print_bit:
     	# Print a single bit
     	li $v0, 11
-        syscall   
-    	# Decrement counter by one
-    	addi $t1, $t1, -1
-    	# Shift left to get next bit
-    	sll $t0, $t0, 1
-    	# Loop until counter is zero 
-    	bnez $t1, print_binary
+        syscall       	
+    	addi $t1, $t1, -1            # Decrement counter by one    	
+    	sll $t0, $t0, 1              # Shift left to get next bit    	
+    	bnez $t1, print_binary_loop  # Loop until counter is zero 
         
         
     print_new_line:
@@ -76,31 +67,24 @@
         
           
     set_reverse_variables:
-        # Reset counter for 16 bits
-        li $t1, 16
-        # Mask to extract each bit 
-        li $t2, 1
-        # Initialize decimal value of reverse binary
-        li $t6, 0
-        # Mask to extract decimal value of reverse binary
-    	li $t7, 0x8000
+        li $t1, 16      # Reset counter for 16 bits      
+        li $t2, 0x0001  # Mask to extract each bit      
+        li $t6, 0       # Initialize decimal value of reverse binary      
+    	li $t7, 0x8000  # Mask to extract decimal value of reverse binary
     
-    print_binary_reverse:
-	# Extract a bit using AND operation
-        and $t3, $t4, $t2
-        # Check if bit is equal to zero
-        beqz $t3, load_zero_reverse
-        # If the bit value is not zero, load one
-    	li $a0, '1'
-    	or $t6, $t6, $t7
+    
+    print_binary_reverse_loop:	
+        and $t3, $t4, $t2            # Extract a bit using AND operation        
+        beqz $t3, load_zero_reverse  # Check if bit is equal to zero       
+    	li $a0, '1'                  # If the bit value is not zero, load one   	
+    	or $t6, $t6, $t7             # Add to decimal value using OR operation
      	j print_bit_reverse
 
     
-    load_zero_reverse:
-    	li $a0, '0'
-    	srl $t7, $t7, 1
+    load_zero_reverse:   	
+    	li $a0, '0'      # Load zero     	
+    	srl $t7, $t7, 1  # Shift right decimal mask
     	
-
 
     print_bit_reverse:
     	# Print a single bit
@@ -111,7 +95,7 @@
     	# Shift right to get next bit
     	srl $t4, $t4, 1
     	# Loop until counter is zero 
-    	bnez $t1, print_binary_reverse
+    	bnez $t1, print_binary_reverse_loop
     	
     	
     print_another_new_line:
