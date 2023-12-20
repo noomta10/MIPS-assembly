88a48f52185562119df78de474d3527af74add7f
noam <noomta10@gmail.com>
2023-12-20 15:34:19 +0200

    DONE

--- mmn11_q3.asm
+++ mmn11_q3.asm
@@ -8,28 +8,24 @@
 # $t6 = mask to extract bits for decimal print
 
 .macro  print_string(%x)
+	# Print new line
 	li $v0,11
 	li $a0,'\n'
         syscall
+        # Print string
 	li $v0, 4
 	la $a0, %x
 	syscall
-.end_macro
-
-.macro print_new_line  
-	li $v0,11
-	li $a0,'\n'
-        syscall 
 .end_macro
 
 
 .data
     prompt: .asciiz "Enter a number between -9999 and 9999:\n"
     invalid_prompt: .asciiz "Invalid number. number must be between -9999 and 9999:\n"
-    new_line: .asciiz "\n"
     binary_prompt: .asciiz "Binary representation in 16 bits: "
     reverse_binary_prompt: .asciiz "Reverse binary representation in 16 bits: "
     decimal_prompt: .asciiz "Decimal value of reverse binary: "
+    
     
 .text
     main:
@@ -45,8 +41,7 @@
         # Store the number in $t0, $t4 and $t7
         move $t0, $v0
         move $t4, $t0
-
-                
+          
         blt $t0, -9999, invalid_input  # Branch if less than -9999	
         bgt $t0, 9999, invalid_input   # Branch if grater than 9999
       
@@ -79,9 +74,9 @@
     	bnez $t1, print_binary_loop  # Loop until counter is zero 
         
     
-    #print_new_line
     print_string(reverse_binary_prompt)  
-              
+    
+                        
     set_reverse_variables:
         li $t1, 16      # Reset counter for 16 bits      
         li $t2, 0x0001  # Mask to extract each bit      
@@ -112,8 +107,8 @@
     	bnez $t1, print_binary_reverse_loop  # Loop until counter is zero 
     	
     	
-    #print_new_line
     print_string(decimal_prompt)	
+    
     
     get_decimal: 
     	andi $t3, $t5, 0x8000 # Put MSB to $t3
@@ -126,7 +121,8 @@
     
     
     print_decimal:
-    	or $t3, $t3, $t5
+    	or $t3, $t3, $t5 # Sign extend
+    	# Print decimal value
     	li $v0, 1
     	move $a0, $t3
     	syscall
