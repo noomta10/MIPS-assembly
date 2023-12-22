b665a2f80dadaf24c6c8d90e7ba94ce234b29f5d
noam <noomta10@gmail.com>
2023-12-22 17:07:05 +0200

    continue with getting input

--- mmn11_q4.asm
+++ mmn11_q4.asm
@@ -17,9 +17,9 @@
 
 .data
     input_prompt: .asciiz "Enter 3 different digits:\n"
-    invalid_input_prompt: "Invalid digit. Digit must be between 0 and 9:\n"
+    invalid_character_prompt: "Invalid digit. Digit must be between 0 and 9:\n"
     done_getting_input_prompt: "Done Getting input\n"
-    bool: .space 4
+    bool: .space 3
 
 
 .text
@@ -27,9 +27,9 @@
     	la $a0, bool  # Put bool in $a0 for get_number use
     	li $t1, 0 # Initialize digits counter to 0
     	   	
-    get_number:
+    get_numbers:
         	print_string(input_prompt)
-    	move $t0, $a0 # Copy bool array to $t0    	
+    	move $t0, $a0 # Copy bool array to $t0  
     	j get_digit_loop   	
     
     get_digit_loop:
@@ -48,20 +48,29 @@
     	blt $t2, '0', invalid_input   	
 	      	
     valid_digit:
-        sb $t2, ($t0) # Store digit in bool array
+        sb $t2, 0($t0) # Store digit in bool array
     	addi $t0, $t0, 1 # Move to next position in the array
     	addi $t1, $t1, 1 # Add one to digits counter
     	j get_digit_loop
   
-   done_getting_input:
+    done_getting_input:
    	print_string(done_getting_input_prompt)
-   	print_string(bool)
+      	# Print the value stored in the register
+   	 addi $t0, $t0, -3 # Go back to beginning of bool
+   	 # Store digits in registers
+   	 lb $t3, 0x00($t0)
+   	 lb $t4, 0x01($t0)
+   	 lb $t5, 0x02($t0)
+   	 # Check uniqueness
+   	 beq $t3, $t4, get_numbers 
+   	 beq $t3, $t5, get_numbers     
+   	 beq $t4, $t5, get_numbers 
    	j exit_program
-   
-   invalid_input:
-    	print_string(invalid_input_prompt)
+      
+    invalid_input:
+    	print_string(invalid_character_prompt)
     	j get_digit_loop
     	
-   exit_program:
+    exit_program:
    	li $v0, 10
    	syscall
--- try.asm
+++ try.asm
@@ -1,31 +1,25 @@
-.macro  print_string(%x)
-    # Print new line
-    li $v0,11
-    li $a0,'\n'
-    syscall
-    # Print string
-    li $v0, 4
-    la $a0, %x
-    syscall
-.end_macro
+    .data
+array:  .space 3    # Allocate space for 3 characters
 
+    .text
+    .globl main
 
-.data
-    input_prompt: .asciiz "Enter 3 different digits:\n"
+main:
+    li $v0, 12          # syscall code 12 for reading a character
+    li $t0, 3           # Counter to read 3 characters
+    la $t1, array       # Load address of array into $t1
 
-.text
-    get_number:
-        	print_string(input_prompt)
-        	li $v0, 12    	
-    	syscall 
-    	move $t2, $v0
-    
-     print_char:       
-	# Print char
-    	li $v0, 11
-    	move $a0, $t2
-    	syscall
-    	 # Print new line
-   	li $v0,11
-    	li $a0,'\n'
-        j get_number 
+read_loop:
+    syscall             # Read a character
+    sb $v0, ($t1)       # Store the character in the array
+    addi $t0, $t0, -1   # Decrement counter
+    addi $t1, $t1, 1    # Move to the next element in the array
+    bnez $t0, read_loop # Loop until 3 characters are read
+
+    # Printing the array
+    li $v0, 4           # syscall code 4 for printing string
+    la $a0, array       # Load address of array into $a0
+    syscall             # Print the array
+
+    li $v0, 10          # syscall code 10 for exit
+    syscall             # Exit the program
